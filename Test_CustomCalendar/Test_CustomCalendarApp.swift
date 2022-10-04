17f1c16d086654de099644e1edd43bfbd9e05e15
JTrojanowicz <jaroslaw.trojanowicz@gmail.com>
2022-10-04 09:18:31 +0200

    Initial Commit

---
+++ Test_CustomCalendar/Test_CustomCalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_CustomCalendarApp.swift
+//  Test_CustomCalendar
+//
+//  Created by Jaroslaw Trojanowicz on 04/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct Test_CustomCalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
